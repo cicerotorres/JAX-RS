973d0cf2597f157467f06066ab9eeaab3478ebd7
Cicero Torres <cicero@Dev.local>
2017-12-13 15:06:29 -0300

    Incluindo parâmetros nos resources e modificando os testes

--- loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -43,7 +43,7 @@ public class ClienteTest {
 	public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/projetos").request().get(String.class);
+		String conteudo = target.path("/projetos/1").request().get(String.class);
 		Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
 
 	}
@@ -52,7 +52,7 @@ public class ClienteTest {
 	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/carrinhos").request().get(String.class);
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 
@@ -62,7 +62,7 @@ public class ClienteTest {
 	public void testaQueRetornaUmCarrinho() {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/projetos").request().get(String.class);
+		String conteudo = target.path("/projetos/1").request().get(String.class);
 		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
 
 		Assert.assertEquals(1, projeto.getId());
