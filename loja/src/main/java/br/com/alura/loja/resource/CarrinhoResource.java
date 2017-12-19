a6678fb7a7ffc8a2735c55752d0860f77849deef
Cicero Torres <cicero@Dev.local>
2017-12-19 14:11:18 -0300

    Adicionando o método que altera um produto

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -51,4 +53,24 @@ public class CarrinhoResource {
 		new CarrinhoDAO().remove(id);
 		return Response.ok().build();
 	}
+
+	/* Teste realizado pelo curl:
+	 * curl -v -X PUT -H "Content-Type: application/xml" 
+	 * -d "<br.com.alura.loja.modelo.Produto> 
+	 * <id>3467</id>
+	 * <quantidade>5</quantidade>
+	 * </br.com.alura.loja.modelo.Produto>" 
+	 * http://localhost:8080/carrinhos/1/produtos/3467/quantidade
+	 */
+	@Path("{id}/produtos/{produtoId}/quantidade")
+	@PUT
+	public Response alteraProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId, String conteudo) {
+		
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		Produto produto = (Produto) new XStream().fromXML(conteudo);
+		carrinho.trocaQuantidade(produto);
+		
+		return Response.ok().build();
+	}
+	
 }
